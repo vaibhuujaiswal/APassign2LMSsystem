e4501efd3af44cd4b38c1120f9e78fcd5b02c2cf
Vaibhav Jaiswal <vaibhav20547@iiitd.ac.in>
2021-10-17 17:31:43 +0530

    changes to addition menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,16 +7,40 @@ public class Main {
         switch(printOption){
             case 1 :
                 System.out.println("Welcome to Backpack\n" +
-                "1. Enter as instructor\n" +
-                "2. Enter as student\n" +
-                "3. Exit\n");
+                        "1. Enter as instructor\n" +
+                        "2. Enter as student\n" +
+                        "3. Exit");
                 break;
             case 2 :
-                System.out.println("");
+                System.out.println("INSTRUCTOR MENU\n" +
+                        "1. Add class material\n" +
+                        "2. Add assessments\n" +
+                        "3. View lecture materials\n" +
+                        "4. View assessments\n" +
+                        "5. Grade assessments\n" +
+                        "6. Close assessment\n" +
+                        "7. View comments\n" +
+                        "8. Add comments\n" +
+                        "9. Logout");
+                break;
+            case 3:
+                System.out.println("STUDENT MENU\n" +
+                        "1. View lecture materials\n" +
+                        "2. View assessments\n" +
+                        "3. Submit assessment\n" +
+                        "4. View grades\n" +
+                        "5. View comments\n" +
+                        "6. Add comments\n" +
+                        "7. Logout");
+                break;
         }
     }
 
     static void studentMenu (){
+        boolean studentCondition = true;
+        while(studentCondition){
+
+        }
 
     }
     static void instructorMenu (){
@@ -31,15 +55,16 @@ public class Main {
 	        int menuOption = scan.nextInt();
             switch(menuOption) {
                 case 1:
-                    studentMenu();
+                    instructorMenu();
                     break;
                 case 2:
-                    instructorMenu();
+                    studentMenu();
+                    break;
                 case 3:
                     backPackOption = false;
                     break;
                 default:
-                    System.out.println("Please enter the correct value for the menu");
+                    System.out.println("Please enter the correct value for the menu!");
                 }
             }
     }
