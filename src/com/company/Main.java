c48d1a43ded29ea71d830efabf30148cc79d1418
Vaibhav Jaiswal <vaibhav20547@iiitd.ac.in>
2021-10-18 15:47:36 +0530

    updated slide addition, and stored time of upload in the form of a string

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,102 @@
 package com.company;
 
-import java.util.Scanner;
+import java.util.*;
+
+class Student {
+    private String studentName;
+    private int studentID;
+
+}
+class Instructor{
+    private String instructorName;
+    private int instructorID;
+}
+class Slides implements View{
+    private String topicOfSlides;
+    private ArrayList<String> contentOfSlides;
+    private String timeOfAddSlide;
+
+    public Slides(String topicOfSlides, ArrayList<String> contentOfSlides, String timeOfAddSlide) {
+        this.topicOfSlides = topicOfSlides;
+        this.contentOfSlides = contentOfSlides;
+        this.timeOfAddSlide = timeOfAddSlide;
+    }
+
+    public String getTopicOfSlides() {
+        return topicOfSlides;
+    }
+
+    public ArrayList<String> getContentOfSlides() {
+        return contentOfSlides;
+    }
+
+    public String getTimeOfAddSlide() {
+        return timeOfAddSlide;
+    }
+    @Override
+    public void view(){
+        this.getTopicOfSlides();
+        this.getContentOfSlides();
+        this.getTimeOfAddSlide();
+    }
+
+
+}
+
+class Video implements View {
+    private String topicOfVideo;
+    private String fileName;
+    private String timeOfAddVideo;
+
+    public Video(String topicOfVideo, String fileName, String timeOfAddVideo) {
+        this.topicOfVideo = topicOfVideo;
+        this.fileName = fileName;
+        this.timeOfAddVideo = timeOfAddVideo;
+    }
+
+    public String getTopicOfVideo() {
+        return topicOfVideo;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getTimeOfAddVideo() {
+        return timeOfAddVideo;
+    }
+
+    @Override
+    public void view(){
+        this.getTopicOfVideo();
+        this.getFileName();
+        this.getTimeOfAddVideo();
+    }
+}
+
+class LectureMaterial {
+
+
+}
+
+class assessmentMaterial{
+
+}
+
+class Comments{ //it would contain instructor or student ID, the comment, and time stamp
+
+}
+
+interface View{
+    public void view(); //will help us view the required functions
+}
 
 public class Main {
+    private static ArrayList<Student> studentList = new ArrayList<Student>();
+    private static ArrayList<Instructor> instructorList = new ArrayList<Instructor>();
+    private static ArrayList<Slides> slideList = new ArrayList<Slides>();
+   private static ArrayList<Video> videoList = new ArrayList<Video>();
+
     static void printMenu(int printOption){
         switch(printOption){
             case 1 :
@@ -40,15 +134,47 @@ public class Main {
     static void instructorMenu (){
         boolean instructorCondition = true;
         while(instructorCondition){
-            printMenu(2);
             Scanner scan = new Scanner(System.in);
+            int instructorID = scan.nextInt();
+            //print option for instructor ID to choose from
             int instructorOption = scan.nextInt();
+            printMenu(2);
             switch (instructorOption){
                 case 1 : //Add class material
+                    ArrayList<String> tempContentArrayList = new ArrayList<String>();
+                    System.out.println("1. Add Lecture Slide\n" +
+                            "2. Add Lecture Video");
+                    int lectureMaterial = scan.nextInt();
+                    switch (lectureMaterial){
+                        case 1: //add lecture slides
+                            System.out.println("Enter topic of slides: ");
+                            String topicLecture = scan.next();
+                            System.out.println("Enter number of slides: ");
+                            int numOfSlides = scan.nextInt();
+                            if (numOfSlides <= 0){
+                                System.out.println("INVALID entry! Number of slides should be greater than zero. Try again!");
+                                break;
+                            }
+                            for (int i = 0; i < numOfSlides; i++ ){
+                                System.out.println("Content of slide " + (i+1) + " : ");
+                                String content = scan.next();
+                                tempContentArrayList.add(content);
+                            }
+
+                            Date date=new Date(); //Stores the time component in form of a string. This will be used later to be presented
+                            String timeOfSlideUpload = date.toString();
+                            slideList.add(new Slides(topicLecture,tempContentArrayList,timeOfSlideUpload));
+                            break;
+                        case 2: //add lecture video
+                            System.out.println("Enter topic of video: ");
+                            String topicVideo;
+                            break;
+                    }
                     break;
                 case 2: // Add assessments
                     break;
                 case 3: //View lecture materials
+
                     break;
                 case 4: //View assessments
                     break;
@@ -103,10 +229,10 @@ public class Main {
             printMenu(1);
 	        int menuOption = scan.nextInt();
             switch(menuOption) {
-                case 1:
+                case 1: //choose instructor menu
                     instructorMenu();
                     break;
-                case 2:
+                case 2: //choose student menu
                     studentMenu();
                     break;
                 case 3:
